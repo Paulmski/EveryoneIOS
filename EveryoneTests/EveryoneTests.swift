5ec04572d24c8b37692442def5d755da382571d6
Paul Molczanski <paulmski@gmail.com>
2022-09-22 08:31:02 -0400

    Test getting employee details

--- EveryoneTests/EveryoneTests.swift
+++ EveryoneTests/EveryoneTests.swift
@@ -18,19 +18,25 @@ class EveryoneTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+ 
+    
+    func testGetEmployee() throws {
+        let webHandler = WebHandler()
+        let expectation = self.expectation(description: "decoding")
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        webHandler.getEmployeeDetails(employerUuid: UUID(uuidString: "d92eb9dc-566d-4427-87c3-ee14c19b4d14")!, id: 2) { employee in
+            XCTAssertEqual(employee?.name, "Toby")
+            XCTAssertEqual(employee?.genderIdentity, "Genderfluid")
+            XCTAssertEqual(employee?.sexualOrientation, "Bisexual")
+            XCTAssertEqual(employee?.ethnicity, "Arab")
+            XCTAssertEqual(employee?.employerId, 2)
+            XCTAssertEqual(employee?.pronoun, "he/him")
+            expectation.fulfill()
+            
         }
+        waitForExpectations(timeout: 20, handler: nil)
+
     }
 
 }
